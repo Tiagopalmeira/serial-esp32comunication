0955525260115047c4298d36427be16054083188
azpthiago <thiagopazba@gmail.com>
2025-03-10 08:48:12 -0300

    Fix: error in shell commands

--- driver/run_cli.py
+++ driver/run_cli.py
@@ -2,13 +2,13 @@ import subprocess
 
 def executar_comando(comando):
     try:
-        resultado = subprocess.run(comando, capture_output=True, text=True, check=True)
-        print("SAIDA DO PROCESSO")
+        resultado = subprocess.run(comando, capture_output=True, text=True, check=True, shell=True)
+        print("----- SAIDA DO PROCESSO -----\n")
         print(resultado.stdout)
     except subprocess.CalledProcessError as e:
         print(f"Erro ao executar o comando: {e}")
         print(f"Saída de erro: {e.stderr}")
 
 # Exemplo de uso
-comando = ["python3", "main.py", "mock.json", "config"]
+comando = "dir"
 executar_comando(comando)
